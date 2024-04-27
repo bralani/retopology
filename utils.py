e9b1e9c3c74674c8dbb3b9986ea312af00c742f9
Matteo Balice <matteobalice02@gmail.com>
2024-04-27 20:36:32 +0200

    refactoring files 2

--- utils.py
+++ utils.py
@@ -1,5 +1,12 @@
 import numpy as np
 import math, os
+import trimesh
+import numpy as np
+import networkx as nx
+import pyvista
+import pyacvd
+import random
+import os
 
 
 # read the obj file and return the vertices and faces
@@ -156,6 +163,157 @@ def generate_output(url_triangle, url_quads):
 
     return np.array(orientation_fields)
 
+def read_file_output(file_path):
+    vertici = []
+    lati = []
+
+    with open(file_path, 'r') as file:
+        for line in file:
+            tokens = line.split()
+
+            if not tokens:
+                continue
+
+            # Estrai vertici
+            if tokens[0] == 'v':
+                vertice = list(map(float, tokens[1:]))
+                vertici.append(vertice)
+
+            # Estrai lati
+            elif tokens[0] == 'l':
+                faccia = [int(vertex.split('/')[0]) for vertex in tokens[1:]]
+                lati.append(faccia)
+
+    return vertici, lati
+
+def faces_to_edges(faces):
+    edges = []
+    for face in faces:
+        for i in range(len(face)):
+            edge = [face[i], face[(i+1)%len(face)]]
+            edges.append(edge)
+    return edges
+
+def rotate(url):
+
+    # Carica il file OBJ
+    vertices, faces = read_obj(url)
+
+    # Estrai i vertici dalla scena OBJ
+    vertices = np.array(vertices)
+
+    # Genera angoli casuali per le rotazioni su tutti e tre gli assi
+    theta_x = np.random.uniform(0, 2 * np.pi)
+    theta_y = np.random.uniform(0, 2 * np.pi)
+    theta_z = np.random.uniform(0, 2 * np.pi)
 
 
+    # Matrici di rotazione attorno agli assi
+    rotation_matrix_x = np.array([[1, 0, 0],
+                                [0, np.cos(theta_x), -np.sin(theta_x)],
+                                [0, np.sin(theta_x), np.cos(theta_x)]])
 
+    rotation_matrix_y = np.array([[np.cos(theta_y), 0, np.sin(theta_y)],
+                                [0, 1, 0],
+                                [-np.sin(theta_y), 0, np.cos(theta_y)]])
+
+    rotation_matrix_z = np.array([[np.cos(theta_z), -np.sin(theta_z), 0],
+                                [np.sin(theta_z), np.cos(theta_z), 0],
+                                [0, 0, 1]])
+
+    rotated_vertices = vertices
+    rotated_vertices = np.dot(rotated_vertices, rotation_matrix_x.T)
+    rotated_vertices = np.dot(rotated_vertices, rotation_matrix_y.T)
+    rotated_vertices = np.dot(rotated_vertices, rotation_matrix_z.T)
+
+    rotated_vertices = scale(rotated_vertices)
+
+
+    # read the file 
+    file = open(url, "r")
+    lines = file.readlines()
+    file.close()
+
+    # write the new file
+    file = open(url, "w")
+    idx = 0
+    for line in lines:
+        if line.startswith('v '):
+            vertex = rotated_vertices[idx]
+            file.write("v " + str(vertex[0]) + " " + str(vertex[1]) + " " + str(vertex[2]) + "\n")
+            idx += 1
+        else:
+            file.write(line)
+
+
+def scale(vertices):
+
+    # Genera fattori di scala uguali per tutti e tre gli assi
+    scale_factors = np.random.uniform(0.5, 2.0)
+
+    # Applica la scala ai vertici
+    scaled_vertices = vertices * scale_factors
+
+    return scaled_vertices
+
+def rotate_scale(url):
+    print("Augmenting " + url + " ...")
+
+    rotate(url)
+
+def generate_trimesh(url):
+
+    print("Preprocessing " + url + " ...")
+    # take the name of the file
+    file_name = os.path.basename(url).split('.')[0]
+    #file_name = url.split('/')[-1].split('.')[0]
+
+    vertices_output, faces_output = read_obj(url)
+    edges_output = faces_to_edges(faces_output)
+    vertices_output.insert(0, [0, 0, 0])
+
+    mesh = pyvista.read(url)
+
+    subdivisions = [random.randint(1,3)]
+    clusters = [random.randrange(10000, 20001)]
+    idx = 1
+    for subdivide in subdivisions:
+        for cluster in clusters:
+            remeshing(mesh, vertices_output, edges_output, file_name, subdivide, cluster, idx)
+            idx += 1
+
+
+def remeshing(mesh, vertices_output, edges_output, file_name, subdivide, cluster, idx):
+    clus = pyacvd.Clustering(mesh)
+    clus.subdivide(subdivide)
+    clus.cluster(cluster)
+    
+    # remesh
+    remesh = clus.create_mesh()
+
+    vertices = remesh.points
+    triangles = remesh.faces
+
+
+    # create a graph from the edges and vertices
+    graph_output = nx.Graph()
+    graph_output.add_nodes_from(range(len(vertices_output)))
+    graph_output.add_edges_from(edges_output)
+
+    
+    # construct faces with the right format
+    faces = []
+    i = 0
+    while i < len(triangles):
+        n = triangles[i]
+        faces.append(triangles[i+1:i+n+1])
+        i += n+1
+
+    faces = np.array(faces).tolist()
+
+        
+    mesh = trimesh.Trimesh(vertices=vertices,
+                            faces=faces)
+
+    # save the output
+    trimesh.exchange.export.export_mesh(mesh, 'new_dataset/train/input/triangles/' + file_name + '.obj')
