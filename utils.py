62739201c959afaf037cc495582c8e71a44fb21a
Matteo Balice <matteobalice02@gmail.com>
2024-04-27 20:15:21 +0200

    refactoring files

--- utils.py
+++ utils.py
@@ -1,9 +1,9 @@
 import numpy as np
 import math, os
-from collections import defaultdict
 
 
-def leggi_obj(file_path):
+# read the obj file and return the vertices and faces
+def read_obj(file_path):
     vertici = []
     facce = []
 
@@ -23,83 +23,56 @@ def leggi_obj(file_path):
     return vertici, facce
 
 
+# return the index of the face closest to the vertex
+def closest_face(vertices_triangled, vertices_ground_truth, faces_ground_truth):
 
-def generate_orientation_field(vertices_triangled, vertices_ground_truth, faces_ground_truth):
-
-    # Definisci il vertice e le facce
     faces_coords = [[vertices_ground_truth[i-1] for i in face] for face in faces_ground_truth]
-    # se la faccia è un tris, sostituisci i valori con infinito
+
     for face in faces_coords:
         if len(face) < 4:
             face.append([float('inf'), float('inf'), float('inf')])
 
     baricentri = np.array([np.mean(face, axis=0) for face in faces_coords])
-
-    '''
-    # Calcola la distanza tra il vertice e tutti i vertici della ground truth
-    distanze = np.linalg.norm(np.array(vertices_ground_truth)[:, np.newaxis, :] - np.array(vertices_triangled)[np.newaxis, :, :], axis=2)
-
-    # per ciascun vertice della ground truth, preleva le facce a cui appartiene e sommaci la distanza
-    sum_faces = defaultdict(int)
-    for i, distanza in enumerate(distanze):
-        faces = vertices_to_faces[i + 1]
-        for face in faces:
-            sum_faces[face] += distanza
-
-    for i, face in enumerate(faces_coords):
-        if len(face) < 4:
-            sum_faces[i] = float('inf')'''
     
-    # Calcola la distanza tra il vertice e tutti i baricentri delle facce della ground truth
     distanze = np.linalg.norm(baricentri[:, np.newaxis, :] - np.array(vertices_triangled)[np.newaxis, :, :], axis=2)
-
-    # Trova l'indice della faccia più vicina
     indice_faccia_piu_vicina = np.argmin(distanze, axis=0)
 
     return indice_faccia_piu_vicina
 
 
-def proiezione_su_faccia(vertice, faccia):
-    # Calcola il vettore normale alla faccia
+# project the vertex on the face
+def project_vertex_face(vertice, faccia):
+    # Calculate the normal vector of the face
     v0 = np.array(faccia[0])
     v1 = np.array(faccia[1])
     v2 = np.array(faccia[2])
     normale = np.cross(v1 - v0, v2 - v0)
 
-    # Scegli uno dei punti sulla faccia come punto di riferimento
     punto_di_riferimento = v0
 
-    # Calcola il vettore dal vertice al punto di riferimento sulla faccia
     v_vertice = np.array(vertice)
     vettore_vertice_riferimento = punto_di_riferimento - v_vertice
 
-    # Calcola la distanza lungo il vettore normale dalla faccia al vertice
     distanza_normale = np.dot(vettore_vertice_riferimento, normale) / np.linalg.norm(normale)
 
-    # Calcola la posizione della proiezione sulla faccia
     proiezione = vertice + (distanza_normale * normale)
 
     return proiezione
 
-def distanza_vertice_a_segmento(vertice, punto1, punto2):
+# calculate the distance between a vertex and a segment
+def distance_vertex_from_segment(vertice, punto1, punto2):
     punto1 = np.array(punto1)
     punto2 = np.array(punto2)
-    # Calcola il vettore dal primo punto del segmento al vertice
-    v_vertice_punto1 = vertice - punto1
 
-    # Calcola il vettore direzione del segmento
+    v_vertice_punto1 = vertice - punto1
     v_direzione = punto2 - punto1
 
-    # Calcola la lunghezza del segmento
     lunghezza_segmento = np.linalg.norm(v_direzione)
 
-    # Normalizza il vettore direzione del segmento
     v_direzione_normalizzato = v_direzione / lunghezza_segmento
 
-    # Calcola la proiezione del vertice sul segmento
     proiezione = np.dot(v_vertice_punto1, v_direzione_normalizzato) / lunghezza_segmento
 
-    # Gestisci i casi in cui la proiezione si trova al di fuori del segmento
     if proiezione < 0:
         punto_prossimo = punto1
     elif proiezione > 1:
@@ -107,11 +80,11 @@ def distanza_vertice_a_segmento(vertice, punto1, punto2):
     else:
         punto_prossimo = punto1 + proiezione * v_direzione_normalizzato
 
-    # Calcola la distanza tra il vertice e il punto più vicino sul segmento
     distanza = np.linalg.norm(vertice - punto_prossimo)
 
     return distanza
 
+# convert the cartesian coordinates to spherical coordinates
 def cartesian_to_spherical(vet):
 
     # normalize the vector
@@ -132,7 +105,6 @@ def generate_orientations(file):
   orientation_fields = generate_output(file_triangle, file)
   orientation_fields_reshaped = orientation_fields.reshape(orientation_fields.shape[0], -1)
 
-
   # save the orientation fields in a txt file
   output_file = "./new_dataset/test/output/" + file_name
   np.savetxt(output_file, orientation_fields_reshaped)
@@ -143,7 +115,7 @@ def generate_orientations(file):
 
 def generate_output(url_triangle, url_quads):
 
-    vertices_ground_truth, faces = leggi_obj(url_quads)
+    vertices_ground_truth, faces = read_obj(url_quads)
 
     vertices_to_faces = {}
     for i, face in enumerate(faces):
@@ -154,33 +126,29 @@ def generate_output(url_triangle, url_quads):
                 vertices_to_faces[vertex] = [i]
 
 
-    vertices_triangle, _ = leggi_obj(url_triangle)
+    vertices_triangle, _ = read_obj(url_triangle)
 
     orientation_fields = []
 
-    indice_faccia = generate_orientation_field(vertices_triangle, vertices_ground_truth, faces)
+    indice_faccia = closest_face(vertices_triangle, vertices_ground_truth, faces)
 
     for idx_vertice, _ in enumerate(vertices_triangle):
         idx_faccia = indice_faccia[idx_vertice]
         faccia_coords = [vertices_ground_truth[i-1] for i in faces[idx_faccia]]
 
-        p_primo = proiezione_su_faccia(vertices_triangle[idx_vertice], [faccia_coords[0], faccia_coords[1], faccia_coords[2]])
+        # project the vertex on the face
+        p_primo = project_vertex_face(vertices_triangle[idx_vertice], [faccia_coords[0], faccia_coords[1], faccia_coords[2]])
 
-        d1 = distanza_vertice_a_segmento(p_primo, faccia_coords[0], faccia_coords[1])
-        d2 = distanza_vertice_a_segmento(p_primo, faccia_coords[1], faccia_coords[2])
-        d3 = distanza_vertice_a_segmento(p_primo, faccia_coords[2], faccia_coords[3])
-        d4 = distanza_vertice_a_segmento(p_primo, faccia_coords[3], faccia_coords[0])
+        d1 = distance_vertex_from_segment(p_primo, faccia_coords[0], faccia_coords[1])
+        d2 = distance_vertex_from_segment(p_primo, faccia_coords[1], faccia_coords[2])
+        d3 = distance_vertex_from_segment(p_primo, faccia_coords[2], faccia_coords[3])
+        d4 = distance_vertex_from_segment(p_primo, faccia_coords[3], faccia_coords[0])
 
         # calculate the frame field directions u and v
         faccia_coords = np.array(faccia_coords)
         u = d3/(d1+d3) * (faccia_coords[1] - faccia_coords[0]) + d1/(d1+d3) * (faccia_coords[2] - faccia_coords[3])
         v = d4/(d2+d4) * (faccia_coords[2] - faccia_coords[1]) + d2/(d2+d4) * (faccia_coords[3] - faccia_coords[0])
 
-        # rendi unitari i vettori u e v
-        #u_normalized = u / np.linalg.norm(u)
-        #v_normalized = v / np.linalg.norm(v)
-
-        # converti i vettori u e v in coordinate sferiche
         u = cartesian_to_spherical(u)
         v = cartesian_to_spherical(v)
 
